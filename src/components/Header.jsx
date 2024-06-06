b629eb4e28fd560bef84632be676f6d3baa2baff
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2024-06-06 13:47:16 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+    return(
+        <>
+            <header>
+                <h1><img id="logo-header" src="cab-logo.png" alt="" /></h1>
+                <ul>
+                    <li><a className="link" href="#">Partidas</a></li>
+                    <li><a className="link" href="#">Elenco</a></li>
+                    <li><a className="link" href="#">Entre em Contato</a></li>
+                </ul>
+                
+            </header>
+            <div className="border-gradient"></div>
+        </>
+    )
+}
